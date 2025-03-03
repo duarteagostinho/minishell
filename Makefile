aac17c8240fccae4fee001500feb4e162439eeb0
Gloryboyduda <duarteaagostinho@gmail.com>
2025-03-03 15:11:20 +0000

    THE START

---
+++ Makefile
@@ -0,0 +1,53 @@
+# Variables
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+SRCS = $(wildcard *.c src/*.c)
+OBJS = $(SRCS:.c=.o)
+NAME = minishell
+
+# ft_printf variables
+FT_PRINTF_DIR = ft_printf
+FT_PRINTF_SRCS = $(wildcard $(FT_PRINTF_DIR)/*.c)
+FT_PRINTF_OBJS = $(FT_PRINTF_SRCS:.c=.o)
+FT_PRINTF_LIB = $(FT_PRINTF_DIR)/libftprintf.a
+
+# libft variables
+LIBFT_DIR = libft
+LIBFT_SRCS = $(wildcard $(LIBFT_DIR)/*.c)
+LIBFT_OBJS = $(LIBFT_SRCS:.c=.o)
+LIBFT_LIB = $(LIBFT_DIR)/libft.a
+
+# Rules
+all: $(NAME)
+
+$(LIBFT_LIB): $(LIBFT_OBJS)
+	ar rcs $(LIBFT_LIB) $(LIBFT_OBJS)
+
+$(FT_PRINTF_LIB): $(FT_PRINTF_OBJS)
+	ar rcs $(FT_PRINTF_LIB) $(FT_PRINTF_OBJS)
+
+$(FT_PRINTF_LIB):
+	$(MAKE) -C $(FT_PRINTF_DIR)
+
+$(LIBFT_LIB):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(NAME): $(OBJS) $(FT_PRINTF_LIB) $(LIBFT_LIB)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) -L$(FT_PRINTF_DIR) -lftprintf -L$(LIBFT_DIR) -lft
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+	$(MAKE) -C $(FT_PRINTF_DIR) clean
+	$(MAKE) -C $(LIBFT_DIR) clean
+
+fclean: clean
+	rm -f $(NAME)
+	$(MAKE) -C $(FT_PRINTF_DIR) fclean
+	$(MAKE) -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ header.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 16:50:34 by duandrad          #+#    #+#             */
+/*   Updated: 2025/03/03 14:49:29 by duandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HELPERS_H
+# define HELPERS_H
+
+/* ANSY COLOURS */
+# define BHRED "\e[1;91m"
+# define BHGRN "\e[1;92m"
+
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
