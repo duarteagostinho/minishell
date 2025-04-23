ffe3cb8f77bdd5a403a08ffb50d8e9dddbeeb9a4
Gloryboyduda <duarteaagostinho@gmail.com>
2025-04-23 18:24:32 +0100

    structures update

--- header.h
+++ header.h
@@ -1,27 +1,109 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   helpers.h                                          :+:      :+:    :+:   */
+/*   header.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:50:34 by duandrad          #+#    #+#             */
-/*   Updated: 2025/03/03 14:49:29 by duandrad         ###   ########.fr       */
+/*   Updated: 2025/04/23 18:16:52 by duandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HELPERS_H
 # define HELPERS_H
 
-# include "ft_printf/ft_printf.h"
-# include "libft/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 # include <stdarg.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "get_next_line/get_next_line.h"
+# include <stdbool.h>
+
+typedef char* string;
+
+typedef struct	s_lexer_list
+{
+	string				str;
+	t_token				type;
+	int					index;
+	struct s_lexer_list	*prev;
+	struct s_lexer_list	*next;
+}	t_lexer_list;
+
+typedef	struct	s_info
+{
+	int			wstatus;
+	t_shell		*shell;
+	int			stdin_copy;
+	pid_t * 	pids;
+}	t_info;
+
+typedef struct	s_shell
+{
+	string					rl_input;
+	string					rl_copy;
+	struct s_environ_list	*environ;
+	struct s_lexer_list		*lex_head;
+	struct s_executor		*executor;
+}	t_shell;
+
+typedef enum	s_token
+{
+	NONE = 0,
+	PIPE = 1,
+	TRUNCATE = 2,
+	APPEND = 3,
+	REDIRECT_INPUT = 4,
+	HEREDOC = 5
+}	t_token;
+
+typedef struct s_environ_node
+{
+	string	key;
+	string	value;
+	struct s_environ_node	*next;
+}	t_environ_node;
+
+typedef struct s_executor
+{
+	int		id;
+	int		size;
+	int		fd_in;
+	int		fd_out;
+	bool	truncate;
+	bool	append;
+	bool	redirect_input;
+	bool	heredoc;
+	string	path;
+	string	execs;
+	struct s_executor	prev;
+	struct s_executor	*next;
+}	t_executor;
+
+typedef struct s_expander
+{
+	string	beginning;
+	string	res;
+	string	invalid;
+	string	env_val;
+	string	var;
+	string	str;
+	string	conv;
+	int		pos;
+	int		len;
+	int		recently_invalid;
+	int		expanded;
+	int		flag;
+}	t_expander;
+
+typedef struct s_environ_list
+{
+	struct s_environ_node	*head;
+	int						size;
+}	t_environ_list;
+
 
 char  *readl_prompt(char  *prompt);
 
--- parsing.c
+++ parsing.c
@@ -3,24 +3,85 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: duandrad <duandrad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 14:20:18 by duandrad          #+#    #+#             */
-/*   Updated: 2025/03/10 15:52:58 by duandrad         ###   ########.fr       */
+/*   Updated: 2025/04/23 18:18:53 by duandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-char  *readl_prompt(char  *prompt)
+char	*readl_prompt(char  *prompt)
 {
-  char  *line;
+	char	*line;
 
-  line = NULL;
-  if (!prompt || !*prompt)
-      return (NULL);
-  line = readline(prompt);
-  if (!line || !*line)
-    return (NULL);
-  return (line);
+	line = NULL;
+	if (!prompt || !*prompt)
+			return (NULL);
+	line = readline(prompt);
+	if (!line || !*line)
+		return (NULL);
+	return (line);
+}
+#include <unistd.h>
+
+typedef char  *string;
+
+
+void mark_pipes(string line, char x)
+{
+	int i;
+	int c;
+
+	c = 0;
+	i = -1;
+
+	while (line[++i])
+	{
+		if ((line[i] == '"' || line[i] == '\'') && !c)
+			c = line[i];
+		else if (c == line[i])
+			c = 0;
+		else if (line[i] == x && !c)
+			line[i] = 2;
+	}
+
+}
+
+typedef enum s_type{
+	HEREDOC,
+	CREATE,
+	APPENDE,
+	READ,
+}	t_type;
+
+typedef struct s_fd {
+	int fd;
+	string file;
+	t_type type;
+	struct  s_fd *next;
+	
+}		t_fd;
+
+typedef struct s_cmd {
+	string line;
+	string *matrix;
+	int write;
+	int read;
+
+
+	t_fd *fd;
+	struct s_cmd *next;
+}		t_cmd;
+
+
+string *parsed(string line)
+{
+	string *matix;
+
+	if (*line)
+		return (NULL);
+	mark_pipes(line);
+	matrix = ft_split(line, 2)
 }
