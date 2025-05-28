b1c8060aed8a72289eb4cc28817f0b4b0394efb6
frank-person <miguelrapphenriques@gmail.com>
2025-05-28 17:06:32 +0100

    changed headerfile

---
+++ src/unset_builtin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset_builtin.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrapp-he <mrapp-he@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/28 16:43:32 by mrapp-he          #+#    #+#             */
+/*   Updated: 2025/05/28 17:03:22 by mrapp-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishell.h"
+
+void	del_var(t_shell *shell, string arg)
+{
+
+}
+
+void	unset_arg(t_shell *shell, string arg, array fails)
+{
+
+}
+
+int	unset(t_shell *shell, vector args)
+{
+
+}
