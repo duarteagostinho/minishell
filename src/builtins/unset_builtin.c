ac44d36bbb422a3085d50b34d6bf7e4be337c617
frank-person <miguelrapphenriques@gmail.com>
2025-05-29 19:54:58 +0100

    finished unset built-in

--- src/builtins/unset_builtin.c
+++ src/builtins/unset_builtin.c
@@ -10,19 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../lib/minishell.h"
+#include "../../lib/minishell.h"
 
 void	del_var(t_shell *shell, string arg)
 {
+	t_env_node	*dummy;
+	t_env_node	*current;
+	t_env_node	*temp;
 
+	dummy = malloc(sizeof(t_env_node));
+	dummy->next = shell->env->head;
+	current = dummy;
+	while (current->next)
+	{
+		if (!ft_strcmp(arg, current->next->key))
+		{
+			temp = current->next->next;
+			free(current->next);
+			current->next = temp;
+			break ;
+		}
+		current = current->next;
+	}
+	shell->env->head = dummy->next;
 }
 
 void	unset_arg(t_shell *shell, string arg, array fails)
 {
-
+	if (arg && (ft_isalpha(*arg) || *arg == '_'))
+		del_var(shell, arg);
+	else
+		fails++;
 }
 
 int	unset(t_shell *shell, vector args)
 {
+	int	fails;
 
+	fails = 0;
+	while (++args)
+		unset_arg(shell, *args, &fails);
+	if (fails > 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
